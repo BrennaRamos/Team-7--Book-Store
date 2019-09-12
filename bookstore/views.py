8a37ca5ba997169c5b8eb5740abcc799271f2905
BrennaRamos <54994475+BrennaRamos@users.noreply.github.com>
2019-09-12 08:59:54 -0400

    Update views.py

--- bookstore/views.py
+++ bookstore/views.py
@@ -25,3 +25,13 @@ def home(request):
     return render(request,'home.html',context)
 def about(request):
     return render(request,'about.html',{'title':'About'})
+
+# Placeholder Views
+#def book(request):
+    #return render(request,'book_detail.html',{'title':'Book Details'})
+#def author(request):
+    #return render(request,'author_detail.html',{'title':'Author Details'})
+#def wishlist(request):
+    #return render(request,'wishlist_detail.html',{'title':'Wishlist Details'})
+#def user(request):
+    #return render(request,'user_detail.html',{'title':'User Details'})
