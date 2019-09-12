e132b848de8dbbe4e3ec8cb635c316d6e890fad7
BrennaRamos <54994475+BrennaRamos@users.noreply.github.com>
2019-09-12 09:01:51 -0400

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='bookstore-home'),
     path('about/', views.about, name='bookstore-about')
-    # path('<int:book_id>/book', views.book_detail, name = 'book_detail')    -- these paths are placeholders as ideas
-    # path('<int:author_id>/author', views.author_detail, name = 'author_detail')
-    # path('<int:wishlist_id>/wishlist', views.wishlist_detail, name = 'wishlist_detail')
-    # path('<int:user_id>/user', views.user_detail, name = 'user_detail')
+    # path('<int:book_id>/book/', views.book_detail, name = 'book_detail')    -- these paths are placeholders as ideas
+    # path('<int:author_id>/author/', views.author_detail, name = 'author_detail')
+    # path('<int:wishlist_id>/wishlist/', views.wishlist_detail, name = 'wishlist_detail')
+    # path('<int:user_id>/user/', views.user_detail, name = 'user_detail')
 ]
