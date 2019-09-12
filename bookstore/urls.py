e0a9699cf7c154100f7b15af661840042031bf0d
BrennaRamos <54994475+BrennaRamos@users.noreply.github.com>
2019-09-12 08:55:07 -0400

    Added placeholder paths, TBD

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='bookstore-home'),
     path('about/', views.about, name='bookstore-about')
+    # path('<int:book_id>/', views.book_detail, name = 'book_detail')    -- these paths are placeholders as ideas
+    # path('<int:author_id>/', views.author_detail, name = 'author_detail')
+    # path('<int:wishlist_id>/', views.wishlist_detail, name = 'wishlist_detail')
+    # path('<int:user_id>/', views.user_detail, name = 'user_detail')
 ]
