6043213e00d036c10163be52b767e5a4cb8321d9
BrennaRamos <54994475+BrennaRamos@users.noreply.github.com>
2019-09-12 09:33:40 -0400

    Added app labels to url groups

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,8 +4,26 @@ from . import views
 urlpatterns = [
     path('', views.home, name='bookstore-home'),
     path('about/', views.about, name='bookstore-about')
-    # path('<int:book_id>/book/', views.book_detail, name = 'book_detail')    -- these paths are placeholders as ideas
-    # path('<int:author_id>/author/', views.author_detail, name = 'author_detail')
-    # path('<int:wishlist_id>/wishlist/', views.wishlist_detail, name = 'wishlist_detail')
-    # path('<int:user_id>/user/', views.user_detail, name = 'user_detail')
 ]
+
+ # These paths are placeholders as ideas
+
+#app_name = 'book'
+#urlpatterns = [
+    #path('<int:book_id>/book/', views.book_detail, name = 'book_detail')
+#]
+
+#app_name = 'author'
+#urlpatterns = [
+    #path('<int:author_id>/author/', views.author_detail, name = 'author_detail')
+#]
+
+#app_name = 'wishlist'
+#urlpatterns = [
+    # path('<int:wishlist_id>/wishlist/', views.wishlist_detail, name = 'wishlist_detail')
+#]
+
+#app_name = 'user'
+#urlpatterns = [
+    # path('<int:user_id>/user/', views.user_detail, name = 'user_detail')
+#]
