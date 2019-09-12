ccc2cd8b5695e70cf26b92fa12185e348199a888
Carlos Rossi <carlos.rossi16@gmail.com>
2019-09-11 23:53:19 -0400

    some basic bs

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'Carlos',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'August 1, 2019'
+    },
+    
+    {
+        'author':'Jane',
+        'title':'Blog Post 2',
+        'content':'First post content',
+        'date_posted':'August 2, 2019'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    context  = {
+        'posts':posts
+    }
+    return render(request,'home.html',context)
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request,'about.html',{'title':'About'})
