724d97f12ffbebaccee757ea981e1d1e192c176b
DianaCNunez <54996627+DianaCNunez@users.noreply.github.com>
2019-09-15 15:19:12 -0400

    Add Sprint 1 files via upload
    Added cart app. Also, edited master url and setting files to include cart app info.

---
+++ bookstore/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='bookstore-home'),
+    path('books/', views.books, name='bookstore-books')
+]
+
+ # These paths are placeholders as ideas
+
+#app_name = 'book'
+#urlpatterns = [
+    #path('<int:book_id>/book/', views.book_detail, name = 'book_detail')
+#]
+
+#app_name = 'author'
+#urlpatterns = [
+    #path('<int:author_id>/author/', views.author_detail, name = 'author_detail')
+#]
+
+#app_name = 'wishlist'
+#urlpatterns = [
+    # path('<int:wishlist_id>/wishlist/', views.wishlist_detail, name = 'wishlist_detail')
+#]
+
+#app_name = 'user'
+#urlpatterns = [
+    # path('<int:user_id>/user/', views.user_detail, name = 'user_detail')
+#]
---
+++ bookstore/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'title':'Book 1',
+        'author':'James Doe',
+        'datePublished':'August 1, 2019'
+    },
+    
+    {
+        'title':'Book 2',
+        'author':'Jane Doe',
+        'datePublished':'August 2, 2019'
+    }
+]
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def books(request):
+    context  = {
+        'posts':posts,
+        'title':'Books'
+    }
+
+# Placeholder Views
+#def book(request):
+    #return render(request,'book_detail.html',{'title':'Book Details'})
+#def author(request):
+    #return render(request,'author_detail.html',{'title':'Author Details'})
+#def wishlist(request):
+    #return render(request,'wishlist_detail.html',{'title':'Wishlist Details'})
+#def user(request):
+    #return render(request,'user_detail.html',{'title':'User Details'})
