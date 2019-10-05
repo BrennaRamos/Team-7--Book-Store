d06c130ccd9ee64866dff8bae39588b09b17411c
BrennaRamos <bramo033@fiu.edu>
2019-10-05 12:41:20 -0400

    'images_fixed'

--- team_project/urls.py
+++ team_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from reviews import views as reviews_views
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +27,7 @@ urlpatterns = [
     path('', include ('reviews.urls')),
     path('', include ('wishlist.urls')),
     path('', include ('account.urls')),
-]
+] 
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
