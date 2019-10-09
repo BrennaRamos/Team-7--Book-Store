0b1612d9a8bca8579d400fc65bd8456c8e8389d6
yesly <yesly.rodriguez@gmail.com>
2019-10-08 23:32:16 -0400

    users can review a book only once

--- reviews/views.py
+++ reviews/views.py
@@ -8,22 +8,28 @@ from django.contrib import messages
 from django.db.models import Avg
 
 def reviews(request, id, title):
+	user = request.user
 	book = get_object_or_404(Book, id=id, title=title)
 	reviews = Review.objects.filter(book=book).order_by('-id')
+	this_user_review = reviews.filter(author=user)
+	reviews_minus_this_user = reviews.exclude(author=user)
 	average_rating = reviews.aggregate(average=Avg('rating'))
 	if request.method == 'POST':
-		user = request.user
 		if user.is_authenticated:
-			review_form = ReviewForm(request.POST or None)
-			if review_form.is_valid() and Book_User.objects.filter(book=book, user=user).exists():
-				content = request.POST.get('content')
-				rating = request.POST.get('rating')
-				review = Review.objects.create(book=book, author=user, content=content, rating=rating)
-				review.save()
-				messages.success(request, "You've successfully reviewed this book.")
-				return HttpResponseRedirect(request.path_info)
+			if Review.objects.filter(book=book, author=user).exists():
+				messages.warning(request, "You have already reviewed this book.")
+				review_form = ReviewForm(request.POST or None)
 			else:
-				messages.warning(request, "You must purchase this book in order to review it.")
+				review_form = ReviewForm(request.POST or None)
+				if review_form.is_valid() and Book_User.objects.filter(book=book, user=user).exists():
+					content = request.POST.get('content')
+					rating = request.POST.get('rating')
+					review = Review.objects.create(book=book, author=user, content=content, rating=rating)
+					review.save()
+					messages.success(request, "You've successfully reviewed this book.")
+					return HttpResponseRedirect(request.path_info)
+				else:
+					messages.warning(request, "You must purchase this book in order to review it.")
 		else:
 			messages.warning(request, "You must be logged in to write a review.")
 			review_form = ReviewForm(request.POST or None)
@@ -32,9 +38,12 @@ def reviews(request, id, title):
 
 	context = {
     	'title': 'Reviews',
+    	'this_user_review': this_user_review,
+    	'reviews_minus_this_user': reviews_minus_this_user,
     	'reviews': reviews,
     	'review_form': review_form,
     	'book_title': book.title,
-    	'average_rating': average_rating['average']
+    	'average_rating': average_rating['average'],
+    	'user': user,
     }
 	return render(request, 'reviews/reviews.html', context)
