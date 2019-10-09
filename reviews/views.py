59a1fee23776b17e2f6f8ee3afcd61aa58afadfc
yesly <yesly.rodriguez@gmail.com>
2019-10-08 23:57:55 -0400

    fixing errors

--- reviews/views.py
+++ reviews/views.py
@@ -11,36 +11,39 @@ def reviews(request, id, title):
 	user = request.user
 	book = get_object_or_404(Book, id=id, title=title)
 	reviews = Review.objects.filter(book=book).order_by('-id')
-	this_user_review = reviews.filter(author=user)
-	reviews_minus_this_user = reviews.exclude(author=user)
 	average_rating = reviews.aggregate(average=Avg('rating'))
+
+	if user.is_authenticated:
+		this_user_review = reviews.filter(author=user)
+		reviews_minus_this_user = reviews.exclude(author=user)
+	else:
+		this_user_review =  None
+		reviews_minus_this_user = reviews
+		review_form = ReviewForm(request.POST or None)
+
 	if request.method == 'POST':
-		if user.is_authenticated:
-			if Review.objects.filter(book=book, author=user).exists():
-				messages.warning(request, "You have already reviewed this book.")
-				review_form = ReviewForm(request.POST or None)
-			else:
-				review_form = ReviewForm(request.POST or None)
-				if review_form.is_valid() and Book_User.objects.filter(book=book, user=user).exists():
-					content = request.POST.get('content')
-					rating = request.POST.get('rating')
-					review = Review.objects.create(book=book, author=user, content=content, rating=rating)
-					review.save()
-					messages.success(request, "You've successfully reviewed this book.")
-					return HttpResponseRedirect(request.path_info)
-				else:
-					messages.warning(request, "You must purchase this book in order to review it.")
-		else:
-			messages.warning(request, "You must be logged in to write a review.")
+		if Review.objects.filter(book=book, author=user).exists():
+			messages.warning(request, "You have already reviewed this book.")
 			review_form = ReviewForm(request.POST or None)
+		else:
+			review_form = ReviewForm(request.POST or None)
+			if review_form.is_valid() and Book_User.objects.filter(book=book, user=user).exists():
+				content = request.POST.get('content')
+				rating = request.POST.get('rating')
+				review = Review.objects.create(book=book, author=user, content=content, rating=rating)
+				review.save()
+				messages.success(request, "You've successfully reviewed this book.")
+				return HttpResponseRedirect(request.path_info)
+			else:
+				messages.warning(request, "You must purchase this book in order to review it.")
 	else:
 		review_form= ReviewForm()
 
 	context = {
     	'title': 'Reviews',
+    	'reviews': reviews,
     	'this_user_review': this_user_review,
     	'reviews_minus_this_user': reviews_minus_this_user,
-    	'reviews': reviews,
     	'review_form': review_form,
     	'book_title': book.title,
     	'average_rating': average_rating['average'],
