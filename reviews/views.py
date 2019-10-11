9f559143a56e07c10593d00d2543cf542cdef41d
yesly <yesly.rodriguez@gmail.com>
2019-10-11 00:25:11 -0400

    improving the warning

--- reviews/views.py
+++ reviews/views.py
@@ -12,30 +12,33 @@ def reviews(request, id, title):
 	book = get_object_or_404(Book, id=id, title=title)
 	reviews = Review.objects.filter(book=book).order_by('-id')
 	average_rating = reviews.aggregate(average=Avg('rating'))
-
 	if user.is_authenticated:
 		this_user_review = reviews.filter(author=user)
 		reviews_minus_this_user = reviews.exclude(author=user)
+		purchased = Book_User.objects.filter(book=book, user=user).exists()
+		reviewed_already = Review.objects.filter(book=book, author=user).exists()
 	else:
 		this_user_review =  None
 		reviews_minus_this_user = reviews
 		review_form = ReviewForm(request.POST or None)
+		purchased = None
+		reviewed_already = None
 
 	if request.method == 'POST':
 		if Review.objects.filter(book=book, author=user).exists():
-			messages.warning(request, "You have already reviewed this book.")
+			messages.warning(request, "You can only review a book once.")
 			review_form = ReviewForm(request.POST or None)
 		else:
 			review_form = ReviewForm(request.POST or None)
-			if review_form.is_valid() and Book_User.objects.filter(book=book, user=user).exists():
+			if review_form.is_valid():
 				content = request.POST.get('content')
 				rating = request.POST.get('rating')
 				review = Review.objects.create(book=book, author=user, content=content, rating=rating)
 				review.save()
 				messages.success(request, "You've successfully reviewed this book.")
-				return HttpResponseRedirect(request.path_info)
+				review_form= ReviewForm()
 			else:
-				messages.warning(request, "You must purchase this book in order to review it.")
+				review_form= ReviewForm()
 	else:
 		review_form= ReviewForm()
 
@@ -48,5 +51,7 @@ def reviews(request, id, title):
     	'book_title': book.title,
     	'average_rating': average_rating['average'],
     	'user': user,
+    	'purchased': purchased,
+    	'reviewed_already': reviewed_already,
     }
 	return render(request, 'reviews/reviews.html', context)
