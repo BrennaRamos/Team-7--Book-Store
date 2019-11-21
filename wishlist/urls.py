4543f65c0c9a18ab41293f71c53764b2141b10d0
bpala012 <54583636+bpala012@users.noreply.github.com>
2019-11-21 05:44:04 -0500

    Wishlist Name Form (WIP)

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,7 +4,7 @@ from wishlist.views import WishlistNameList
 
 urlpatterns = [
     path('wishlist/', views.wishlist, name='wishlist'),
-    path('wishlist/rename_wish', views.rename_wish, name = 'rename_wish'),
+    path('wishlists/rename_wish', views.rename_wish, name = 'rename_wish'),
     path('wishlists/', WishlistNameList.as_view(), name= 'wishlists'),
     path('addtowish/<int:book_id>/<int:homeNum>',views.add_wish, name='addtowish'),
     path('transwish/<int:entrie_id>/<int:newHome>',views.trans_wish, name='transwish'),
